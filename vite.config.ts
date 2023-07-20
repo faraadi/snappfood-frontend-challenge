410b517edeb3737ab69a7a53e2a3f94223ff18e0
Farhad Sharifi <faradidev@gmail.com>
2023-07-20 23:36:40 +0330

    chore: update vite version

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		react()
+		react({ include: /\.(jsx|ts|tsx)$/ })
 	],
+	build: {
+		emptyOutDir: true
+	},
 	publicDir: path.resolve(__dirname, 'public'),
+	server: {
+		port: 3000
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
