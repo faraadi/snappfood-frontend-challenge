79356098cfcd9843fc24d95a91dc3a92f53ef7b4
Farhad Sharifi <faradidev@gmail.com>
2023-07-20 23:11:44 +0330

    chore: update vite config to support public dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import path from 'path';
+import * as path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react()
+	],
+	publicDir: path.resolve(__dirname, 'public'),
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
